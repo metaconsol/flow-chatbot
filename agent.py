4b9ceed099f00d2aaaa8bac65a80e4c8c620aba7
MetaConsol <metaconsol@gmail.com>
2026-03-26 12:48:13 +0530

    fix: added google.generative as genai

--- agent.py
+++ agent.py
@@ -1,26 +1,67 @@
 
 
-import os
-from dotenv import load_dotenv
-from google import genai
+# import os
+# from dotenv import load_dotenv
+# from google import genai
 
+# load_dotenv()
+
+# #  Create client (new way)
+# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# def generate_response(user_message, order_data, context):
+
+#     prompt = f"""
+# You are the official assistant for the Mrignayanee Buyer App.
+
+# Rules:
+# - Answer ONLY from the given context
+# - Do not hallucinate
+# -if someone asks explicitly who created you, answer "I was created by the Abuzar Maroof."
+# - Be clear and concise
+# - If answer not found in context, say "Sorry, I don't have that information.
+# - Always write in that language that the user used in their question."
+
+# Context:
+# {context}
+
+# Customer Question:
+# {user_message}
+
+# Answer:
+# """
+
+#     response = client.models.generate_content(
+#         model="gemini-2.5-flash",   
+#         contents=prompt
+#     )
+
+#     return response.text
+
+
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+# from google import genai
 load_dotenv()
 
-#  Create client (new way)
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
 
 def generate_response(user_message, order_data, context):
 
     prompt = f"""
 You are the official assistant for the Mrignayanee Buyer App.
+ Mrignayanee is the official e-commerce platform of the Government of Madhya Pradesh promoting authentic handloom and handicraft products made by local artisans.
 
 Rules:
 - Answer ONLY from the given context
 - Do not hallucinate
--if someone asks explicitly who created you, answer "I was created by the Abuzar Maroof."
+- if someone asks explicitly who created you, answer "I was created by the Abuzar Maroof."
 - Be clear and concise
-- If answer not found in context, say "Sorry, I don't have that information.
-- Always write in that language that the user used in their question."
+- If answer not found in context, say "Sorry, I don't have that information."
 
 Context:
 {context}
@@ -31,9 +72,6 @@ Customer Question:
 Answer:
 """
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",   
-        contents=prompt
-    )
+    response = model.generate_content(prompt)
 
     return response.text
