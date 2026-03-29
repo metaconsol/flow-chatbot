de1bdc3c97f31216bb60ea10fcfc2ff746f6673e
MetaConsol <metaconsol@gmail.com>
2026-03-29 11:56:40 +0530

    edit passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,17 @@
 import sys
 import os
 
-# Set correct Python path (your venv)
-INTERP = "/home/themrignayanee/public_html/chatbot/flow-chatbot/venv/bin/python"
+INTERP = "/home/themrignayanee/public_html/chatbot/flow-chatbot/env_mrignayanee/bin/python"
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
+
+# Add project path
+sys.path.insert(0, "/home/themrignayanee/public_html/chatbot/flow-chatbot")
 
 # Import FastAPI app
 from main import app
 
-# Convert ASGI -> WSGI
-from asgiref.compatibility import guarantee_single_callable
+# Convert ASGI → WSGI
 from asgiref.wsgi import AsgiToWsgi
 
 application = AsgiToWsgi(app)
